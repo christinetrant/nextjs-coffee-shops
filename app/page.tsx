d6ec7ea36a95f74a62041d60bd3a417d3d81b29f
Christine Trant <christine@umidigital.co.uk>
2025-05-07 17:03:06 +0100

    Amend metadata & add tagline to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Coffee Shops Close By!</h1>
+      <h1>Coffee Shops Nearby</h1>
+      <h2>Discover your local coffee shops</h2>
     </main>
   );
 }
