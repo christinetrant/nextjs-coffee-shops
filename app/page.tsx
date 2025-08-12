a24b71bef7e0a468cb23cf3670bbe8b99330165c
christine <christinetrant@gmail.com>
2025-08-12 12:55:53 +0100

    Add banner client component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
+import Banner from "@/components/banner.client";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Coffee Shops Nearby</h1>
-      <h2>Discover your local coffee shops</h2>
-      <p>test paragraph</p>
+      <Banner />
     </main>
   );
 }
